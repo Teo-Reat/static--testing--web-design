05cbd343b5883f236237ab2060d4780d1163c2bc
Teo-Reat <teo.reat@gmail.com>
2020-05-17 20:42:26 +0300

    Mobile version added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'xsm:': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   variants: {},
